70f304eca21f6f4d2da07f4f6f50a1c66edbd972
Ahmed Said <ahmed.said2290@icloud.com>
2026-09-11 14:19:19 -0400

    feat/updater (#75)
    * updater
    
    * fix lints

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,5 +1,5 @@
 #![expect(clippy::doc_markdown)]
-use std::fs;
+use std::{fs, process};
 
 use anyhow::Context;
 use camino::Utf8PathBuf;
@@ -32,7 +32,42 @@ fn main() -> anyhow::Result<()> {
             targets_options,
             references_options,
         } => convert_target_list_and_reference_datasets(&targets_options, &references_options, &output_dir)?,
-        Command::External(_) => (),
+        Command::Upgrade => {
+            let updater_name = "xenium-panel-convert-update";
+
+            let err_message1 = || format!("failed to run {updater_name}");
+
+            let err_message2 = || {
+                format!(
+                    "failed to upgrade {} - go to {}/releases/latest to reinstall",
+                    env!("CARGO_BIN_NAME"),
+                    env!("CARGO_PKG_REPOSITORY")
+                )
+            };
+
+            let output = process::Command::new(updater_name)
+                .output()
+                .with_context(err_message1)
+                .with_context(err_message2)?;
+
+            let process::Output {
+                status,
+                mut stdout,
+                stderr,
+            } = output;
+
+            if !stdout.is_empty() {
+                stdout.push(b'\n');
+            }
+
+            let message = String::from_utf8(stdout.into_iter().chain(stderr).collect()).unwrap_or_default();
+
+            if status.success() {
+                println!("{message}");
+            } else {
+                eprintln!("{message}");
+            }
+        }
     }
 
     Ok(())
@@ -68,7 +103,6 @@ enum Command {
         #[clap(flatten)]
         references_options: ReferenceDatasetCliOptions,
     },
-    #[expect(dead_code)]
-    #[clap(external_subcommand)]
-    External(Vec<String>),
+    /// Update xp-convert to the latest version (runs xenium-panel-convert-update)
+    Upgrade,
 }
